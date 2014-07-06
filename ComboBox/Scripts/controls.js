2688340dc54bd1d4e060d8cd785a216ba4db3422
TanyaDimitrova <tanya.dimitrova@mail.bg>
2014-07-06 03:18:41 +0300

    Homework

---
+++ ComboBox/Scripts/controls.js
@@ -0,0 +1,51 @@
+﻿define(['jquery', 'handlebars'], function ($) {
+    var ComboBox = (function () {
+
+        function ComboBox(items) {
+            if (!(this instanceof ComboBox)) {
+                return new ComboBox(items);
+            }
+            this._items = items;
+        };
+
+        ComboBox.prototype.render = function render(template) {
+            var $container = $('#combo-box-container')
+                .addClass('collapsed');
+
+            for (var i = 0; i < this._items.length; i++) {
+                var item = $(template(this._items[i]));
+
+                $container.append(item);
+            }
+            var $allPersons = $container.find(".person-item");
+            $allPersons.hide()
+                .first()
+                .addClass('selected')
+                .show();
+
+            $container.on('click', ".person-item", function () {
+                var $this = $(this);
+
+                if ($container.hasClass('expanded')) {
+                    $allPersons.find('.selected').removeClass('selected');
+                    $allPersons.hide();
+                    $this.addClass('selected')
+                        .show();
+                }
+
+                if ($container.hasClass('collapsed')) {
+                    $allPersons.show();
+                }
+            });
+
+            return $container;
+        };
+
+        return ComboBox;
+    }());
+
+    return {
+        ComboBox: ComboBox
+
+    }
+}());
---
+++ ComboBox/Scripts/test.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    require.config({
+        paths: {
+            "jquery": "libs/jquery-2.1.1",
+            "handlebars": "libs/handlebars.min",
+            "controls": "controls"
+        }
+    })
+
+    require(['jquery', 'controls'], function ($, controls) {
+        var people = [{
+
+            id: 1,
+            name: "Doncho Minkov",
+            age: 18,
+            avatarUrl: "images/minkov.jpg"
+        }, {
+            id: 2,
+            name: "Georgi Georgiev",
+            age: 19,
+            avatarUrl: "images/joreto.jpg"
+        }
+        ];
+        var comboBox = controls.ComboBox(people);
+        var template = $("#person-template").html();
+        var comboBoxHtml = comboBox.render(template);
+        container.innerHTML = comboBoxHtml;
+    });
+
+}());
+
